236a4a0ea0278d8d6474adc9eec37f806334feb0
deloooonp <delonpratama1009@gmail.com>
2026-08-29 13:52:32 +0700

    01 introduction

---
+++ 01-introduction/intro.ts
@@ -0,0 +1,9 @@
+const user = { name: "Delon", age: 10 };
+
+console.log("Delon");
+
+console.log(user.name);
+
+// .email will give an error because email property doesnt exist
+// tsc will still work but the code wont run in javascript
+// console.log(user.email);
