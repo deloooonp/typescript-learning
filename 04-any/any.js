1497c75a8480e22613dbc2b67067c61432a357a4
deloooonp <delonpratama1009@gmail.com>
2026-08-29 15:04:09 +0700

    04 any

---
+++ 04-any/any.js
@@ -0,0 +1,7 @@
+"use strict";
+// in this case typescript has no idea on what to infer the value
+let hero;
+function getHero() {
+    return "thor";
+}
+hero = getHero();
---
+++ 04-any/any.ts
@@ -0,0 +1,8 @@
+// in this case typescript has no idea on what to infer the value
+let hero;
+
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
