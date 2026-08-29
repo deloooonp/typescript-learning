690406a8d6ede7d384d0f803377efcf63dac01ac
deloooonp <delonpratama1009@gmail.com>
2026-08-29 14:05:22 +0700

    02 variables

---
+++ 02-variables/variables.js
@@ -0,0 +1,3 @@
+let greetings = "Hello Delon";
+console.log(greetings);
+export {};
---
+++ 02-variables/variables.ts
@@ -0,0 +1,16 @@
+// let variableName: type = value
+let greetings: string = "Hello Delon";
+
+// cant assign the variable to another type since we declared the type as string
+// greetings = 6;
+// greetings = true;
+
+greetings.toLowerCase();
+console.log(greetings);
+
+// Sometimes you dont need to declare the type like : type
+let myNum = 6;
+// this property doesnt exist on the type number (only strings)
+// myNum.toUpperCase();
+
+export {};
