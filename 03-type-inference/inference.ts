e9bcff4647d223a8c4911553f379ac302bc0d266
deloooonp <delonpratama1009@gmail.com>
2026-08-29 14:58:26 +0700

    03 type inference

---
+++ 03-type-inference/inference.ts
@@ -0,0 +1,14 @@
+// typescript is smart enough to know what type the variable is by the value
+// so it automatically declare the types for the variable
+
+// string
+let greetings = "Hello Delon";
+
+// number
+let userId = 12345;
+// userId = "www";
+
+// boolean
+let isLoggedIn = false;
+
+export {};
